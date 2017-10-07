ef57f82839ff5af6dc4fb92ba7e013ca77f06029
NguyenKimDien <dienit02@gmail.com>
2017-10-07 13:53:23 +0700

    cập nhật

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -21,7 +21,7 @@ License: You must have a valid license purchased only from themeforest(the above
 
     <head>
         <meta charset="utf-8" />
-        <title>Metronic | The Ultimate Multi-purpose Bootstrap Admin Dashboard Theme | Theme #1 | Admin Dashboard</title>
+        <title>CRM | Admin Dashboard</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta content="#1 selling multi-purpose bootstrap admin theme sold in themeforest marketplace packed with angularjs, material design, rtl support with over thausands of templates and ui elements and plugins to power any type of web applications including saas and admin dashboards. Preview page of Theme #1 for statistics, charts, recent events and reports"
