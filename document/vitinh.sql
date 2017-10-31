717bdc7179a0d975393dd4ace06060eff44b9691
NguyenKimDien <dienit02@gmail.com>
2017-10-31 18:20:52 +0700

    cap nhat

--- document/vitinh.sql
+++ document/vitinh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 31, 2017 lúc 12:18 PM
+-- Thời gian đã tạo: Th10 31, 2017 lúc 12:20 PM
 -- Phiên bản máy phục vụ: 10.1.22-MariaDB
 -- Phiên bản PHP: 7.1.4
 
